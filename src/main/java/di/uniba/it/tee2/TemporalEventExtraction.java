a90cece38c84a8e3ebdb73be8655835c1e1b9aaf
Pierpaolo Basile <pierpaolo.basile@gmail.com>
2014-07-17 19:48:36 +0200

    - New indexing method

--- src/main/java/di/uniba/it/tee2/TemporalEventExtraction.java
+++ src/main/java/di/uniba/it/tee2/TemporalEventExtraction.java
@@ -36,8 +36,11 @@ public class TemporalEventExtraction {
     private TemporalExtractor tempExtractor;
     private FSDirectory time_index;
     private FSDirectory doc_index;
+    private FSDirectory docrep_index;
     private IndexWriter time_writer;
     private IndexWriter doc_writer;
+    private IndexWriter docrep_writer;
+    private int contextSize = 256;
     private static final Logger logger = Logger.getLogger(TemporalEventExtraction.class.getName());
 
     /**
@@ -59,6 +62,9 @@ public class TemporalEventExtraction {
         IndexWriterConfig configDoc = new IndexWriterConfig(Version.LUCENE_48, analyzer);
         configDoc.setOpenMode(IndexWriterConfig.OpenMode.CREATE_OR_APPEND);
         doc_writer = new IndexWriter(doc_index, configDoc);
+        IndexWriterConfig configDocRep = new IndexWriterConfig(Version.LUCENE_48, analyzer);
+        configDoc.setOpenMode(IndexWriterConfig.OpenMode.CREATE_OR_APPEND);
+        docrep_writer = new IndexWriter(docrep_index, configDocRep);
 
     }
 
@@ -70,6 +76,15 @@ public class TemporalEventExtraction {
         //close writers
         time_writer.close();
         doc_writer.close();
+        docrep_writer.close();
+    }
+
+    public int getContextSize() {
+        return contextSize;
+    }
+
+    public void setContextSize(int contextSize) {
+        this.contextSize = contextSize;
     }
 
     /**
@@ -88,36 +103,55 @@ public class TemporalEventExtraction {
             logger.log(Level.WARNING, "Error to process doc " + docID + " (skip doc)", ex);
         }
         if (tt != null) {
-            //crea i documenti per i due indici di lucene
-            Document time_doc = new Document();
-            Document doc_doc = new Document();
 
-            //stores id and text (not tagged) in doc_index
+            //stores id and text (not tagged) in docrep_index (document repository)
+            Document docrep_doc = new Document();
+            docrep_doc.add(new StringField("id", docID, Field.Store.YES));
+            docrep_doc.add(new StringField("content", tt.getText(), Field.Store.YES));
+            docrep_writer.addDocument(docrep_doc);
+
+            //stores id and text (not tagged) in doc_index for search
+            Document doc_doc = new Document();
             doc_doc.add(new StringField("id", docID, Field.Store.YES));
-            doc_doc.add(new StringField("content", tt.getText(), Field.Store.YES));
+            doc_doc.add(new TextField("content", tt.getText(), Field.Store.NO));
             doc_writer.addDocument(doc_doc);
 
-            //stores id, file name and text (TimeML tagged) in time_index 
-            time_doc.add(new StringField("id", docID, Field.Store.YES));
-            time_doc.add(new StringField("file", fileName, Field.Store.YES));
-            time_doc.add(new TextField("content", tt.getTaggedText(), Field.Store.NO));
-
             for (TimeEvent event : tt.getEvents()) { //for each TIMEX3 store info time index
-                FieldType ft = new FieldType();
-                ft.setStoreTermVectors(true);
-                ft.setTokenized(true);
-                ft.setStored(true);
-                ft.setIndexed(true);
-                ft.setStoreTermVectorPositions(true);
-                ft.setOmitNorms(false);
+                //stores id, file name and text (TimeML tagged) in time_index
+                Document time_doc = new Document();
+                time_doc.add(new StringField("id", docID, Field.Store.YES));
+                //time_doc.add(new StringField("file", fileName, Field.Store.YES));
+                //time_doc.add(new TextField("content", tt.getTaggedText(), Field.Store.NO));
+                /*FieldType ft = new FieldType();
+                 ft.setStoreTermVectors(true);
+                 ft.setTokenized(true);
+                 ft.setStored(true);
+                 ft.setIndexed(true);
+                 ft.setStoreTermVectorPositions(true);
+                 ft.setOmitNorms(false);*/
 
-                time_doc.add(new Field("time", event.getDateString(), ft));
+                time_doc.add(new StringField("time", event.getDateString(), Field.Store.YES));
                 time_doc.add(new IntField("offset_start", event.getStartOffset(), Field.Store.YES));
                 time_doc.add(new IntField("offset_end", event.getEndOffset(), Field.Store.YES));
+                time_doc.add(new TextField("context", getTimeContext(tt.getText(), event.getStartOffset(), event.getEndOffset()), Field.Store.NO));
+                time_writer.addDocument(time_doc);
             }
-
-            time_writer.addDocument(time_doc);
         }
+    }
+
+    public String getTimeContext(String content, int startOffset, int endOffset) {
+        int start = Math.max(0, startOffset - contextSize);
+        int end = Math.min(content.length(), endOffset + contextSize);
+        String context = content.substring(start, end);
+        int index = context.indexOf(" ");
+        if (index >= 0 && index < context.length()) {
+            context = context.substring(index + 1);
+        }
+        index = context.lastIndexOf(" ");
+        if (index >= 0 && index < context.length()) {
+            context = context.substring(0, index);
+        }
+        return context;
     }
 
     /**
